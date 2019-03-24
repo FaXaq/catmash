e39272699c9ec1ab30a49c1b899e187e34149ca4
Marin Procureur <marin.procureur@gmail.com>
2019-03-24 18:46:25 +0100

    Show all cats in home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="home">
+    <ul>
+      <li v-for="cat in cats" :key="cat.id">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { getCats } from '@/services/api';
+import { ICat } from '@/models/Cat';
+
 @Component({})
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private cats: Array<ICat> = getCats();
+}
 </script>
